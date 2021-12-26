f38f2626c491e559c53333a2b0ac94d1690f91fa
최윤서 <alice78651@gmail.com>
2021-12-27 02:41:21 +0900

    12/27

---
+++ src/com/campus/board/msg/controller/MsgBoardSelectOneServlet.java
@@ -0,0 +1,59 @@
+package com.campus.board.msg.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.campus.board.msg.model.service.MsgBoardService;
+import com.campus.board.msg.model.service.MsgBoardServiceImpl;
+import com.campus.board.msg.model.vo.MsgBoard;
+
+/**
+ * Servlet implementation class MsgBoardSelectOneServlet
+ */
+@WebServlet("/board/msg/selectOne.do")
+public class MsgBoardSelectOneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MsgBoardSelectOneServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int msgNo = Integer.parseInt(request.getParameter("msgNo"));
+		System.out.println(msgNo);
+		
+		MsgBoardService msgbService = new MsgBoardServiceImpl();
+		MsgBoard msgBoard = msgbService.msgboardSelectOne(msgNo);
+		
+		if (msgBoard != null) {
+			RequestDispatcher view = request.getRequestDispatcher("/community/msg/msgpost.jsp");
+			request.setAttribute("msgBoard", msgBoard);
+			view.forward(request, response);
+
+		} else {
+			response.sendRedirect("/community/error.jsp");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
